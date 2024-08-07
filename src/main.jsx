7a2a0e5c6f69222a0dfc0a8abda6ca5fb788a50c
Crno <118383162+Crn0@users.noreply.github.com>
2024-08-07 14:09:02 +0800

    feat: add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
+import Router from './routes/Router'
 import './global-styles/index.css'
 import './global-styles/reset.css'
 import './global-styles/theme.css'
@@ -8,6 +8,6 @@ import './global-styles/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
 )
