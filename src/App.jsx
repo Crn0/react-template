7242d6dbaec45c3ed3eb0486ea8077ec328e781b
crno <118383162+Crn0@users.noreply.github.com.>
2025-03-08 16:00:13 +0800

    refactor: format strings

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [theme, setTheme] = useState('light')
-  const currentTheme = theme === 'light' ? 'dark' : 'light';
+  const [theme, setTheme] = useState("light");
+  const currentTheme = theme === "light" ? "dark" : "light";
 
   return (
-    <div className= {`${currentTheme}`} style={{display: 'grid', placeContent: 'center', height: '100dvh'}}>
-     <header>
-        <button type='button' onClick={() => setTheme((t) => t === 'light' ? 'dark' : 'light')}>
-          { theme === 'light' ? 'dark mode ': 'light mode' }
-         </button>
-     </header>
-      
+    <div
+      className={`${currentTheme}`}
+      style={{ display: "grid", placeContent: "center", height: "100dvh" }}
+    >
+      <header>
+        <button type="button" onClick={() => setTheme((t) => (t === "light" ? "dark" : "light"))}>
+          {theme === "light" ? "dark mode " : "light mode"}
+        </button>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +9,4 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "/test/setup/setup.js",
   },
-})
+});
